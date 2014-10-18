5059164fd7c9ff516813241865b88800f3f4b5c9
System Administrator <root@Andreass-MBP.localdomain>
2014-10-18 17:16:56 +0200

    configuration and test added

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect,
+    spies = require('chai-spies');
+
+describe("Foo", function(){
+  it("should do something meaningfull", function(){
+    expect(true).to.equal(true);
+  });
+});
